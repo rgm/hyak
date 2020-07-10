c0127193724acd38677e27859bd3c7df8be9a87f
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-07-10 13:56:31 +0000

    Add percentage of time test
    We need to allow a bit of slop in the percentage of time gate, since
    it'll be statistically correct over enough runs, but we don't want to
    wait for it in the unit tests.

--- test/hyak/core_test.clj
+++ test/hyak/core_test.clj
@@ -11,8 +11,8 @@
   (defn make-fkey [s] (str PREFIX s))
   (defn clear-test-features! [fstore]
     (let [test-feature? #(string/starts-with? % PREFIX)]
-    (doseq [fkey (filter test-feature? (sut/features fstore))]
-      (sut/remove! fstore fkey)))))
+      (doseq [fkey (filter test-feature? (sut/features fstore))]
+        (sut/remove! fstore fkey)))))
 
 (defn fixture [test-fn]
   (clear-test-features! fs)
@@ -31,7 +31,22 @@
     (sut/disable! fs fkey)
     (is (sut/disabled? fs fkey))))
 
+(deftest percentage-of-time-gate-test
+  (let [fkey       (make-fkey "my_percent_time_test")
+        percentage 25
+        low-end    20 ;; uses (rand), so allow a bit of slop
+        high-end   30]
+    (sut/add! fs fkey)
+    (sut/enable-percentage-of-time! fs fkey percentage)
+    (let [run-count          1000
+          test-fn            #(sut/enabled? fs fkey)
+          all-runs           (take run-count (repeatedly test-fn))
+          successful-runs    (filter true? all-runs)
+          success-proportion (* 100 (/ (count successful-runs) run-count))]
+      (is (< low-end success-proportion high-end)))))
+
 (comment
   (require '[kaocha.repl])
   (clear-test-features! fs)
-  (kaocha.repl/run *ns*))
+  (kaocha.repl/run)
+  (kaocha.repl/run-all))
