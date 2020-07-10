babf0bfd9688649a15203927f4384617b8b810f2
Ryan McCuaig <ryan@ryanmccuaig.net>
2020-07-10 04:20:06 +0000

    Clear test features before tests
    Add a fixture to clear the test-prefixed features from the redis store
    before starting any tests. Do this as part of setup so that we're not
    destroying evidence on test failures by doing it in teardown.

--- test/hyak/core_test.clj
+++ test/hyak/core_test.clj
@@ -1,22 +1,37 @@
 (ns hyak.core-test
   (:require
    [clojure.string :as string]
-   [clojure.test   :refer :all]
+   [clojure.test   :as t :refer [deftest is]]
    [hyak.core      :as sut]))
+
+(def fs (sut/make-fstore))
 
 ;; prefix all features so we can clean these up in redis
 (let [PREFIX "TEST_FEATURE_"]
-  (defn test-feature? [s] (string/starts-with? s PREFIX))
-  (defn make-fkey [s] (str PREFIX s)))
+  (defn make-fkey [s] (str PREFIX s))
+  (defn clear-test-features! [fstore]
+    (let [test-feature? #(string/starts-with? % PREFIX)]
+    (doseq [fkey (filter test-feature? (sut/features fstore))]
+      (sut/remove! fstore fkey)))))
+
+(defn fixture [test-fn]
+  (clear-test-features! fs)
+  (test-fn))
+
+(t/use-fixtures :each fixture)
 
 ;; gate tests
 
 (deftest boolean-gate-test
-  (let [fs   (sut/make-fstore)
-        fkey (make-fkey "my_bool_feature")]
+  (let [fkey (make-fkey "my_bool_feature")]
     (sut/add! fs fkey)
     (is (sut/disabled? fs fkey))
     (sut/enable! fs fkey)
     (is (sut/enabled? fs fkey))
     (sut/disable! fs fkey)
     (is (sut/disabled? fs fkey))))
+
+(comment
+  (require '[kaocha.repl])
+  (clear-test-features! fs)
+  (kaocha.repl/run *ns*))
